105299fd62833aed4b9b04750cabc450f92ce5eb
Cris Ryan Tan <crisryantan@gmail.com>
2017-02-21 00:07:57 +0800

    added form validation for signup component

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,25 +1,47 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
+const FIELDS = {
+	email : {
+		type  : 'input',
+		label : 'Email :',
+		error : 'Please enter an email.'
+	},
+	password : {
+		type  : 'input',
+		label : 'Password :',
+		error : 'Please enter a password.'
+	},
+	passwordConfirm : {
+		type  : 'input',
+		label : 'Confirm Password:',
+		error : 'Please enter a password confirmation.'
+	}
+}
+
 class Signup extends Component {
+
+	renderField ( fieldConfig, field ) {
+		const fieldHelper = this.props.fields[ field ];
+
+		return (
+			<fieldset className="form-group" key={field}>
+				<label>{ fieldConfig.label }</label>
+				<fieldConfig.type type="text" className="form-control" {...fieldHelper}/>
+				<div className="text-help">
+				{ fieldHelper.touched && fieldHelper.error && <div className="error">{ fieldHelper.error }</div> }
+				</div>
+			</fieldset>
+		);
+	}
 
 	render () {
 		const { handleSubmit, fields : { email, password, passwordConfirm } } = this.props;
 		return (
 			<form>
-				<fieldset className="form-group">
-					<label>Email: </label>
-					<input type="text" className="form-control" {...email}/>
-				</fieldset>
-				<fieldset className="form-group">
-					<label>Password: </label>
-					<input type="password" className="form-control" {...password}/>
-				</fieldset>
-				<fieldset className="form-group">
-					<label>Confirm Password: </label>
-					<input type="password" className="form-control" {...passwordConfirm}/>
-				</fieldset>
+				{ _.map( FIELDS, this.renderField.bind( this ) ) }
 				<button action="submit" className="btn btn-primary">Sign up</button>
 			</form>
 		);
@@ -27,7 +49,25 @@ class Signup extends Component {
 
 }
 
+function validate ( formProps ) {
+	const errors = {};
+
+	_.each( FIELDS, ( type, field ) => {
+		console.log( type )
+		if ( !formProps[ field ] ) {
+			errors[ field ] = type.error;
+		}
+	} );
+
+	if ( formProps.password !== formProps.passwordConfirm ) {
+		errors.password = 'Passwords must match';
+	}
+
+	return errors;
+}
+
 export default reduxForm( {
 	form   : 'signup',
-	fields : [ 'email', 'password', 'passwordConfirm' ]
+	fields : [ 'email', 'password', 'passwordConfirm' ],
+	validate
 } )( Signup );
