60c7440d03f806cef9e5a07e8a9dab858f1d706f
Cris Ryan Tan <crisryantan@gmail.com>
2017-02-21 13:28:14 +0800

    add hoc for routes that needs to be authenticated

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
+import Feature from './components/feature';
+import RequireAuth from './components/auth/require_auth';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware( reduxThunk )(createStore);
@@ -20,6 +22,7 @@ ReactDOM.render(
 				<Route path="signin" component={Signin} />
 				<Route path="signout" component={Signout} />
 				<Route path="signup" component={Signup} />
+				<Route path="feature" component={RequireAuth( Feature )} />
 			</Route>
 		</Router>
 	</Provider>
