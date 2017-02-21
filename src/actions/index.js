f2c1848e90011e12da7dc7db84dc6fa1c91fc94e
Cris Ryan Tan <crisryantan@gmail.com>
2017-02-21 13:12:12 +0800

    implement signup feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -24,6 +24,18 @@ export function signinUser( { email, password } ) {
 	}
 }
 
+export function signupUser ( { email, password } ) {
+	return function ( dispatch ) {
+		axios.post(`${ROOT_URL}/signup`, { email, password } )
+			.then( response => {
+				dispatch( { type : AUTH_USER } );
+				localStorage.setItem( 'token', response.data.token );
+				browserHistory.push( '/feature' );
+			} )
+			.catch( response => dispatch( authError ( 'Email is in use.' ) ) );
+	}
+}
+
 export function authError( error ) {
 	return {
 		type    : AUTH_ERROR,
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -5,23 +5,40 @@ import * as actions from '../../actions';
 
 const FIELDS = {
 	email : {
-		type  : 'input',
-		label : 'Email :',
-		error : 'Please enter an email.'
+		type      : 'input',
+		inputType : 'text',
+		label     : 'Email :',
+		error     : 'Please enter an email.'
 	},
 	password : {
-		type  : 'input',
-		label : 'Password :',
-		error : 'Please enter a password.'
+		type      : 'input',
+		inputType : 'password',
+		label     : 'Password :',
+		error     : 'Please enter a password.'
 	},
 	passwordConfirm : {
-		type  : 'input',
-		label : 'Confirm Password:',
-		error : 'Please enter a password confirmation.'
+		type      : 'input',
+		inputType : 'password',
+		label     : 'Confirm Password:',
+		error     : 'Please enter a password confirmation.'
 	}
 }
 
 class Signup extends Component {
+
+	handleFormSubmit ( formProps ) {
+		this.props.signupUser( formProps );
+	}
+
+	renderAlert () {
+		if ( this.props.errorMessage ) {
+			return (
+				<div className="alert alert-danger">
+					<strong>{ this.props.errorMessage }</strong>
+				</div>
+			);
+		}
+	}
 
 	renderField ( fieldConfig, field ) {
 		const fieldHelper = this.props.fields[ field ];
@@ -29,7 +46,7 @@ class Signup extends Component {
 		return (
 			<fieldset className="form-group" key={field}>
 				<label>{ fieldConfig.label }</label>
-				<fieldConfig.type type="text" className="form-control" {...fieldHelper}/>
+				<fieldConfig.type type={ fieldConfig.inputType } className="form-control" {...fieldHelper}/>
 				<div className="text-help">
 				{ fieldHelper.touched && fieldHelper.error && <div className="error">{ fieldHelper.error }</div> }
 				</div>
@@ -40,8 +57,9 @@ class Signup extends Component {
 	render () {
 		const { handleSubmit, fields : { email, password, passwordConfirm } } = this.props;
 		return (
-			<form>
+			<form onSubmit={ handleSubmit( this.handleFormSubmit.bind( this ) ) }>
 				{ _.map( FIELDS, this.renderField.bind( this ) ) }
+				{ this.renderAlert() }
 				<button action="submit" className="btn btn-primary">Sign up</button>
 			</form>
 		);
@@ -53,7 +71,6 @@ function validate ( formProps ) {
 	const errors = {};
 
 	_.each( FIELDS, ( type, field ) => {
-		console.log( type )
 		if ( !formProps[ field ] ) {
 			errors[ field ] = type.error;
 		}
@@ -66,8 +83,12 @@ function validate ( formProps ) {
 	return errors;
 }
 
+function mapStateToProps ( state ) {
+	return { errorMessage : state.auth.error }
+}
+
 export default reduxForm( {
 	form   : 'signup',
 	fields : [ 'email', 'password', 'passwordConfirm' ],
 	validate
-} )( Signup );
+}, mapStateToProps, actions )( Signup );
